442aa3f129aa85cdd5f1ce9614825b36b4a03e2d
ak1103dev <ak.apichan1103@gmail.com>
2020-07-19 16:09:41 +0700

    :construction: add query of find

--- app/services/user/schema.ts
+++ app/services/user/schema.ts
@@ -3,9 +3,11 @@ import Joi from '@hapi/joi'
 export const createUserSchema = Joi.object({
   email: Joi.string()
     .email()
+    .lowercase()
     .trim()
     .required(),
   username: Joi.string()
+    .lowercase()
     .trim()
     .required(),
   password: Joi.string().required(),
@@ -13,6 +15,7 @@ export const createUserSchema = Joi.object({
 
 export const updateUserSchema = Joi.object({
   username: Joi.string()
+    .lowercase()
     .trim()
     .required(),
 })
@@ -20,7 +23,18 @@ export const updateUserSchema = Joi.object({
 export const loginSchema = Joi.object({
   email: Joi.string()
     .email()
+    .lowercase()
     .trim()
     .required(),
   password: Joi.string().required(),
+})
+
+export const querySchema = Joi.object({
+  limit: Joi.number().optional(),
+  skip: Joi.number().optional(),
+  where: Joi.object({
+    email: Joi.string().optional(),
+    username: Joi.string().optional(),
+  }).optional(),
+  sort: Joi.string().optional(),
 })
