364aade539c7185f8d8272cd8e40917485b93f44
Edward Park <ed.sh.park@gmail.com>
2020-10-07 10:16:04 -0700

    add: swagger endpoint + init

--- app/app.ts
+++ app/app.ts
@@ -1,18 +1,37 @@
 import express from 'express'
 import cors from 'cors'
 
+const SwaggerJSDoc = require('swagger-jsdoc')
+const SwaggerUI = require('swagger-ui-express')
+
 import authentication from 'middlewares/authentication'
 import logErrors from 'middlewares/logErrors'
 import errorHandler from 'middlewares/errorHandler'
 import routes from 'routes'
+import { version } from '../package.json'
 
 const port = process.env.PORT || 8080
 const app: express.Application = express()
+
+const swaggerSpec = SwaggerJSDoc({
+  definition: {
+    info: {
+      title: 'Node TS Boilerplate',
+      description: 'A Node + Typescript boilerplate project',
+      version: version,
+      servers: [`http://localhost:${port}`],
+    },
+  },
+
+  /* Can also configure wildcards within dirs (eg /routes/*.ts) */
+  apis: ['/build/**/*.js'],
+})
 
 app.use(cors())
 app.use(express.json())
 app.use(authentication)
 
+app.use('/documentation', SwaggerUI.serve, SwaggerUI.setup(swaggerSpec))
 app.get('/', function(req, res) {
   res.send({
     title: 'Welcome to server',
--- app/routes/user.ts
+++ app/routes/user.ts
@@ -5,6 +5,24 @@ import { authentication } from 'roleResolvers'
 
 const router = Router()
 
+/**
+ * @swagger
+ *
+ * /api/users:
+ *   post:
+ *     description: Creates a user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: user
+ *         description: User object
+ *         in:  body
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: users
+ */
 router.post('/', userServices.create)
 router.get('/', auth([authentication]), userServices.find)
 router.get('/:id', userServices.userMe, userServices.findById)
